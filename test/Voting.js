6ca1452abbbe91b98d72f8e6215cb4cece98ccda
Sandy Instructor <sandyludosky@gmail.com>
2021-03-01 16:22:46 +0100

    no change

--- test/Voting.js
+++ test/Voting.js
@@ -1,6 +1,15 @@
 const { assert } = require("chai");
-
 const Voting = artifacts.require("Voting");
+
+describe("for testing only", () => {
+    // it('should fail', () => {
+    //     assert.equal(2 + 2, 1)
+    // })
+
+    it('should pass', () => {
+        assert.equal(2 + 2, 4)
+    })
+})
 
 contract("Voting", accounts => {
     let [account1, account2] = accounts;
@@ -12,15 +21,8 @@ contract("Voting", accounts => {
     it("workflow status should be RegisteringVoters", () => {
         return Voting.deployed()
             .then(instance => instance.status())
-            .then(status => assert.equal(status.toNumber(), 0), "status should RegisteringVoters upon deploy")
+            .then(status => assert.equal(status.toNumber(), 0), "status should be RegisteringVoters upon deploy")
 
     })
-
-    // it('should fail', () => {
-    //     assert.equal(2 + 2, 1)
-    // })
-
-    it('should pass', () => {
-        assert.equal(2 + 2, 4)
-    })
+    it("")
 })
